2770bdbb319e75cf56c2cc7ea6522407c54c592f
AhmadAnisAkram <ahmed.anis@wanclouds.net>
2021-01-08 15:25:47 +0500

    app plugin

---
+++ data/plugins/grafana-toolkit/Archive/src/ExampleRootPage.tsx
@@ -0,0 +1,27 @@
+import { AppRootProps } from '@grafana/data';
+import { Device } from './pages/Device';
+import React from 'react';
+// import {pages} from 'pages'
+// import { useNavModel } from 'utils/hooks';
+
+export const ExampleRootPage = React.memo(function ExampleRootPage(props: AppRootProps) {
+  // const {
+  //   path,
+  //   onNavChanged,
+  //   query: { tab },
+  //   meta,
+  // } = props;
+  // Required to support grafana instances that use a custom `root_url`.
+  // const pathWithoutLeadingSlash = path.replace(/^\//, '');
+
+  // // Update the navigation when the tab or path changes
+  // const navModel = useNavModel(
+  //   useMemo(() => ({ tab, pages, path: pathWithoutLeadingSlash, meta }), [meta, pathWithoutLeadingSlash, tab])
+  // );
+  // useEffect(() => {
+  //   onNavChanged(navModel);
+  // }, [navModel, onNavChanged]);
+
+  // const Page = pages.find(({ id }) => id === tab)?.component || pages[0].component;
+  return <Device />;
+});
---
+++ data/plugins/grafana-toolkit/Archive/src/pages/Device.tsx
@@ -0,0 +1,47 @@
+import React, { FC, ReactElement, useState } from 'react';
+import { Tooltip, Button, Typography, Drawer, Divider } from 'antd';
+import { PlusOutlined, PlusCircleOutlined } from '@ant-design/icons';
+import { AddDevice } from './addNewDevice/addDevice';
+import { ListView } from './listview/listview';
+import './styles.less';
+
+export const Device: FC<DeviceProps> = (): ReactElement => {
+  const [visible, setVisible] = useState(false);
+  const toggleDrawer = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <>
+      <div className="listview-container">
+        <div className="titlebar">
+          <h1>Devices</h1>
+          <Divider />
+        </div>
+        <div className="create-button">
+          <Tooltip title='Create'>
+            <span>
+              <Button type="primary" onClick={toggleDrawer} className="listViewButton">
+                <PlusOutlined /><Typography.Text className='create-text'>Create</Typography.Text>
+              </Button>
+            </span>
+          </Tooltip>
+        </div>
+        <Drawer
+          placement="right"
+          width="400px"
+          closable={true}
+          onClose={toggleDrawer}
+          visible={visible}
+        >
+          <AddDevice />
+        </Drawer>
+        <ListView />
+      </div>
+    </>
+  );
+};
+
+type DeviceProps = {
+
+};
